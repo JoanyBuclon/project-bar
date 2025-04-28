947150eec4ab93cdb7636743bdfec13e09310aa9
Joany Buclon <7534658+JoanyBuclon@users.noreply.github.com>
2025-04-28 18:25:04 +0200

    feat: initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,35 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    scanPageMeta: 'after-resolve',
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    normalizeComponentNames: false,
+    spaLoadingTemplateLocation: 'within',
+    parseErrorData: false,
+    pendingWhenIdle: true,
+    defaults: {
+      useAsyncData: {
+        deep: true
+      }
+    }
+  },
+  features: {
+    inlineStyles: true
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false
+    }
+  },
+  devtools: { enabled: true },
+  modules: ['@nuxt/ui', '@nuxt/fonts', '@nuxt/icon'],
+  css: ['~/assets/css/main.css'],
+})
