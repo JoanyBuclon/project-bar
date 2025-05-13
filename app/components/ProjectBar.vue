07b29f5dafe680bb5ffe7854e6643736dee484f6
Joany Buclon <7534658+JoanyBuclon@users.noreply.github.com>
2025-05-13 18:20:37 +0200

    feat: project name input and style

--- app/components/ProjectBar.vue
+++ app/components/ProjectBar.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
-const projectName = ref('Project Name');
-const completionPercent = ref(0);
+    const projectName = ref('Project Name')
+    const completionPercent = ref(0)
 </script>
 
 <template>
     <div class="flex items-center justify-center h-screen">
         <div class="flex flex-col gap-4 w-1/2">
-            <h1 class="text-2xl font-bold self-center">{{ projectName }}</h1>
-            <UProgress v-model="completionPercent" color="primary" size="xl" status />
+            <input v-model="projectName" class="text-4xl font-bold text-center p-6 focus:outline-none" maxlength="40" />
+            <UProgress v-model="completionPercent" color="primary" size="2xl" status />
             <TaskList @percentage="(percentage: number) => completionPercent = percentage" />
         </div>
     </div>
