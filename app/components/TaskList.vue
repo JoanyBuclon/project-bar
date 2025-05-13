0b821e4e2cb50bd38a7fa5c64ecb77ad873c135f
Joany Buclon <7534658+JoanyBuclon@users.noreply.github.com>
2025-05-13 18:21:18 +0200

    feat: update tasks and style

--- app/components/TaskList.vue
+++ app/components/TaskList.vue
@@ -1,54 +1,56 @@
 <script setup lang="ts">
-import type { Task } from '~/types/Task'
+    import type { Task } from '~/types/Task'
 
-const tasks = ref<Task[]>([
-    {
-        name: 'Task 1',
-        completed: true
-    },
-    {
-        name: 'Task 2',
-        completed: false
-    },
-    {
-        name: 'Task 3',
-        completed: false
+    const tasks = ref<Task[]>([
+        {
+            name: 'Task 1',
+            completed: true
+        },
+        {
+            name: 'Task 2',
+            completed: false
+        },
+        {
+            name: 'Task 3',
+            completed: false
+        }
+    ])
+
+    const emit = defineEmits(['percentage'])
+
+    function calculatePercentage() {
+        const completedTasks = tasks.value.filter(task => task.completed).length
+        const percentage = Math.round((completedTasks / tasks.value.length) * 100)
+        emit('percentage', percentage)
+    };
+
+    calculatePercentage();
+
+    // ADD TASK
+    const newTask = ref('')
+    function addTask() {
+        tasks.value.push({ name: newTask.value, completed: false})
+        newTask.value = ''
+        calculatePercentage()
     }
-])
 
-const emit = defineEmits(['percentage'])
-
-function calculatePercentage() {
-    const completedTasks = tasks.value.filter(task => task.completed).length
-    const percentage = Math.round((completedTasks / tasks.value.length) * 100)
-    emit('percentage', percentage)
-};
-
-calculatePercentage();
-
-// ADD TASK
-const newTask = ref('')
-function addTask() {
-    tasks.value.push({ name: newTask.value, completed: false})
-    newTask.value = ''
-    calculatePercentage()
-}
-
-// DELETE TASK
-function deleteTask(task: Task) {
-    tasks.value = tasks.value.filter(t => t !== task)
-    calculatePercentage()
-}
+    // DELETE TASK
+    function deleteTask(task: Task) {
+        tasks.value = tasks.value.filter(t => t !== task)
+        calculatePercentage()
+    }
 </script>
 
 <template>
-    <div v-for="task in tasks" :key="task.name">
-        <UCheckbox v-model="task.completed" :label="task.name" @change="calculatePercentage" />
-        <button @click="deleteTask(task)">Delete</button>
-    </div>
+    <div class="p-6">
+        <div class="flex flex-row group p-2" v-for="task in tasks">
+            <button class="invisible group-hover:visible" @click="deleteTask(task)"><Icon class="text-red-600 cursor-pointer items-center" name="fa6-regular:trash-can" size="16" /></button>
+            <UCheckbox class="items-center transform scale-120 px-4" v-model="task.completed" @change="calculatePercentage" />
+            <input v-model="task.name" class="text-xl font-bold p-2 focus:outline-none" />
+        </div>
 
-    <form @submit.prevent="addTask">
-        <input v-model="newTask" required placeholder="task" />
-        <button>Add Task</button>
-    </form>
+        <form @submit.prevent="addTask">
+            <input class="text-xl font-bold p-2 focus:outline-none" v-model="newTask" required placeholder="Add a new task" />
+        </form>
+    </div>
 </template>
