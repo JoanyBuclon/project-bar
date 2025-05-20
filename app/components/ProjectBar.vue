594c222b203d0854f141ab9f117ce03a78f6b6d9
Joany Buclon <7534658+JoanyBuclon@users.noreply.github.com>
2025-05-20 18:35:55 +0200

    feat: implement data model

--- app/components/ProjectBar.vue
+++ app/components/ProjectBar.vue
@@ -1,18 +1,45 @@
 <script setup lang="ts">
-    const projectName = ref('Project Name')
+    import type { SimpleProject } from '~/types/simple-project'
+    import type { Task } from '~/types/task'
+
+    const project = ref<SimpleProject>({
+        id: 'project',
+        name: 'Project Name',
+        lastUpdate: new Date(),
+        tasks: [
+            {
+                name: 'Task 1',
+                completed: true
+            },
+            {
+                name: 'Task 2',
+                completed: false
+            },
+            {
+                name: 'Task 3',
+                completed: false
+            }
+        ]
+    })
+
     const completionPercent = ref(0)
 
+    function onTasksChange(tasks: Task[]) {
+        const completedTasks = tasks.filter(task => task.completed).length
+        completionPercent.value = Math.round((completedTasks / tasks.length) * 100)
+    }
+
     function checkEmpty() {
-        if (projectName.value.trim() === '') projectName.value = 'Project Name'
+        if (project.value.name.trim() === '') project.value.name = 'Project Name'
     }
 </script>
 
 <template>
     <div class="flex items-center justify-center h-screen">
         <div class="flex flex-col gap-4 w-3/5 lg:w-1/2">
-            <input v-model="projectName" class="text-xl lg:text-4xl font-bold text-center p-2 lg:p-6 focus:outline-none w-full" maxlength="50" @change="checkEmpty" />
+            <input v-model="project.name" class="text-xl lg:text-4xl font-bold text-center p-2 lg:p-6 focus:outline-none w-full" maxlength="50" @change="checkEmpty" />
             <UProgress v-model="completionPercent" color="primary" size="2xl" status />
-            <TaskList @percentage="(percentage: number) => completionPercent = percentage" />
+            <TaskList :tasks="project.tasks" @tasks="onTasksChange" />
         </div>
     </div>
     <Footer />
--- app/components/TaskList.vue
+++ app/components/TaskList.vue
@@ -1,34 +1,19 @@
 <script setup lang="ts">
     import type { Task } from '~/types/task'
 
-    const tasks = ref<Task[]>([
-        {
-            name: 'Task 1',
-            completed: true
-        },
-        {
-            name: 'Task 2',
-            completed: false
-        },
-        {
-            name: 'Task 3',
-            completed: false
-        }
-    ])
+    const props = defineProps({ tasks: Array<Task> })
+    const tasks = ref<Task[]>(props.tasks ?? new Array<Task>());
+    const emit = defineEmits(['tasks'])
 
-    const emit = defineEmits(['percentage'])
-
-    function calculatePercentage() {
-        const completedTasks = tasks.value.filter(task => task.completed).length
-        const percentage = Math.round((completedTasks / tasks.value.length) * 100)
-        emit('percentage', percentage)
+    function tasksChange() {
+        emit('tasks', tasks.value)
     }
 
     function checkEmpty(task: Task) {
         if (task.name.trim() === '') task.name = 'Task'
     }
 
-    calculatePercentage()
+    tasksChange()
 
     const newTask = ref('')
     const addError = ref('')
@@ -41,12 +26,12 @@
         tasks.value.push({ name: newTask.value, completed: false})
         addError.value = ''
         newTask.value = ''
-        calculatePercentage()
+        tasksChange()
     }
 
     function deleteTask(task: Task) {
         tasks.value = tasks.value.filter(t => t !== task)
-        calculatePercentage()
+        tasksChange()
     }
 </script>
 
@@ -56,7 +41,7 @@
             <button class="lg:invisible lg:group-hover:visible" @click="deleteTask(task)">
                 <Icon class="text-red-600 cursor-pointer items-center" name="fa6-regular:trash-can" size="16" />
             </button>
-            <UCheckbox class="items-center transform lg:scale-120 px-2 lg:px-4" v-model="task.completed" @change="calculatePercentage" />
+            <UCheckbox class="items-center transform lg:scale-120 px-2 lg:px-4" v-model="task.completed" @change="tasksChange" />
             <input v-model="task.name" class="text-lg lg:text-xl font-bold p-1 lg:p-2 focus:outline-none w-full" @change="checkEmpty(task)"/>
         </div>
 
