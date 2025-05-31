50792607043046803aaefd99e78f187a69d9aed0
Joany Buclon <7534658+JoanyBuclon@users.noreply.github.com>
2025-05-31 13:22:56 +0200

    fix: project list visuals

--- app/components/ProjectCard.vue
+++ app/components/ProjectCard.vue
@@ -32,7 +32,7 @@
 </script>
 
 <template>
-    <div class="border border-dashed rounded-sm border-current hover:border-primary w-80 h-40 cursor-pointer" @click="onClick">
+    <div class="border-2 border-dashed rounded-lg border-gray-600 hover:border-primary w-80 h-40 cursor-pointer" @click="onClick">
       <div class="w-full h-full flex flex-col gap-4 p-1">
         <div class="flex flex-col gap-2 justify-center items-center h-2/3">
           <p class="font-bold">{{ project.name }}</p>
